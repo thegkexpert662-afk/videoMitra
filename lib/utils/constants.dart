d9b2d900e1cf28e76eb8d21f6bfdd4b00e8681e9
thegkexpert662-afk <thegkexpert662@gmail.com>
2026-09-02 09:43:03 +0530

    refactor: add shared VideoMitra constants

---
+++ lib/utils/constants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class VideoMitraColors {
+  static const background = Color(0xFF050509);
+  static const surface = Color(0xFF12121A);
+  static const accent = Color(0xFFFF2D75);
+  static const purple = Color(0xFF8B3CFF);
+  static const blue = Color(0xFF286BFF);
+}
+
+class VideoMitraDefaults {
+  static const imageDurationSeconds = 5;
+  static const autosaveSeconds = 12;
+  static const maxUndoSteps = 60;
+}
